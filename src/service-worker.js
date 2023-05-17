6c4f3b7f57d98341dc64fa3d13677a7cc52a36fa
Petra-Poliakova <petra.poliakova92@gmail.com>
2023-05-17 22:44:15 +0200

    first commit

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,11 +7,11 @@
 // You can also remove this file if you'd prefer not to use a
 // service worker, and the Workbox build step will be skipped.
 
-import { clientsClaim } from 'workbox-core';
-import { ExpirationPlugin } from 'workbox-expiration';
-import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
-import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate } from 'workbox-strategies';
+import { clientsClaim } from "workbox-core";
+import { ExpirationPlugin } from "workbox-expiration";
+import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import { StaleWhileRevalidate } from "workbox-strategies";
 
 clientsClaim();
 
@@ -24,16 +24,16 @@ precacheAndRoute(self.__WB_MANIFEST);
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
-const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
+const fileExtensionRegexp = new RegExp("/[^/?]+\\.[^/]+$");
 registerRoute(
   // Return false to exempt requests from being fulfilled by index.html.
   ({ request, url }) => {
     // If this isn't a navigation, skip.
-    if (request.mode !== 'navigate') {
+    if (request.mode !== "navigate") {
       return false;
     } // If this is a URL that starts with /_, skip.
 
-    if (url.pathname.startsWith('/_')) {
+    if (url.pathname.startsWith("/_")) {
       return false;
     } // If this looks like a URL for a resource, because it contains // a file extension, skip.
 
@@ -43,16 +43,17 @@ registerRoute(
 
     return true;
   },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+  createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html")
 );
 
 // An example runtime caching route for requests that aren't handled by the
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+  ({ url }) =>
+    url.origin === self.location.origin && url.pathname.endsWith(".png"), // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
-    cacheName: 'images',
+    cacheName: "images",
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
       // least-recently used images are removed.
@@ -63,10 +64,67 @@ registerRoute(
 
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
+  // if (event.data && event.data.type === "CLEAR_CACHE") {
+  //   caches.keys().then((cacheNames) => {
+  //     cacheNames.forEach((cacheName) => {
+  //       caches.delete(cacheName);
+  //     });
+  //   });
+  // }
 });
 
 // Any other custom service worker logic can go here.
+var CACHE_STATIC_NAME = "static-v3";
+var CACHE_DYNAMIC_NAME = "dynamic-v2";
+
+self.addEventListener("install", (event) => {
+  console.log("[Service Worker] Installing Service Worker ...", event);
+  event.waitUntil(
+    caches.open("static-v5").then((cache) => {
+      console.log("[Service Worker] Precaching App Shell");
+      cache.addAll(["/", "/src/app.js"]);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("[Service Worker] Activating Service Worker ....", event);
+  // caches.waitUntil(
+  //   caches.keys().then((keyList) => {
+  //     return Promise.all(
+  //       keyList.map((key) => {
+  //         if (key !== "static-v4" && key !== "dynamic-v3") {
+  //           console.log("[Service Worker] Removing old cache.", key);
+  //           return caches.delete(key);
+  //         }
+  //       })
+  //     );
+  //   })
+  // );
+  return self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      const networkFetch = fetch(event.request)
+        .then((response) => {
+          // update the cache with a clone of the network response
+          const responseClone = response.clone();
+          caches.open("dynamic-v5").then((cache) => {
+            cache.put(event.request.url, responseClone);
+          });
+          return response;
+        })
+        .catch((error) => {
+          console.error("ServiceWorker fetch failed: ", error);
+        });
+      // prioritize cached response over network
+      return cachedResponse || networkFetch;
+    })
+  );
+});
