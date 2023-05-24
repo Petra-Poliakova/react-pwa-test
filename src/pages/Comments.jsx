4ecd0faede0d898e73dad79c716fa347f23a0f58
Petra-Poliakova <petra.poliakova92@gmail.com>
2023-05-24 08:24:11 +0200

    comments styling

--- src/pages/Comments.jsx
+++ src/pages/Comments.jsx
@@ -1,36 +1,37 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
-import Avatar from '../images/avatar.png'
-import '../index.css';
-
+import Avatar from "../images/avatar.png";
+import "../index.css";
 
 function Comments() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(()=>{
-      const fetchData = async() => {
-        const result = await fetch("https://jsonplaceholder.typicode.com/comments").then(
-          response => response.json()
-        )
-        setData(result);
-      }
-      fetchData();
-    }, [])
-    return (
-      <div>
-          <h1>Comments from our users.</h1>
-        {data.map(comment => (
-          <div className='comment-container' key={comment.id}>
-            <div className='avatar-box'><img src={Avatar} alt="user-avatar" /></div>
-            <div className='comment-box'>
-            <h2 className='comment-name'>{comment.name}</h2>
-            <p>{comment.email}</p>
-           <p>{comment.body}</p>
-           </div>
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(
+        "https://jsonplaceholder.typicode.com/comments"
+      ).then((response) => response.json());
+      setData(result);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className="container">
+      <h1>Comments from our users.</h1>
+      {data.map((comment) => (
+        <div className="comment-container" key={comment.id}>
+          <div className="avatar-box">
+            <img src={Avatar} alt="user-avatar" />
           </div>
-        ))}
-      </div>
-    );
-  }
+          <div className="comment-box">
+            <h2 className="comment-name">{comment.name}</h2>
+            <h3>{comment.email}</h3>
+            <p>{comment.body}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
-export default Comments
+export default Comments;
