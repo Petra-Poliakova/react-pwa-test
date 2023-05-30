a4f9f6d98b0155610035af03cd6c15063035affd
Petra poliakova <petra.poliakova@dxc.com>
2023-05-30 16:44:17 +0200

    Farebise database - addDoc, deleteDoc

--- src/pages/Comments.jsx
+++ src/pages/Comments.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, useRef } from "react";
-import db from "../firebase";
-import { uid } from "uid";
-import { set, ref, onValue, remove } from "firebase/database";
+//import db from "../firebase";
+import dbStore from "../firestore";
+//import { uid } from "uid";
+//import { set, ref, onValue, remove } from "firebase/database";
 
 import Avatar from "../images/avatar.png";
 import "../index.css";
+import { collection, onSnapshot, addDoc, doc, deleteDoc } from "firebase/firestore";
 
 function Comments() {
   const [data, setData] = useState([]);
@@ -13,92 +15,61 @@ function Comments() {
   const emailInputRef = useRef();
   const textInputRef = useRef();
 
-  ///// Pre konfiguraciu Firebase
   useEffect(() => {
-    onValue(ref(db), (snapshot) => {
-      const dataDB = snapshot.val();
-      setData([]);
-      console.log("dataDB", dataDB);
+    // Firestore database read
+    onSnapshot(collection(dbStore, "comments"), (snapshot) => {
+         const comments = snapshot.docs.map((comment) => ({...comment.data(), id: comment.id}));
+         //console.log('comments',comments )
+   if (comments !== null) {
+     setData(comments)
+    }
+       });
+      
 
-      if (dataDB !== null) {
-        Object.values(dataDB.comments).map((comment) => {
-          setData((oldComment) => [...oldComment, comment]);
-        });
-      }
+      /*** Pre konfiguraciu Firebase - realtime database****/
+  // onValue(ref(db), (snapshot) => {
+  //   const dataDB = snapshot.val();
+  //   setData([]);
+  //   console.log("dataDB", dataDB);
 
-      //   if(dataDB !== null) {
-      //     const arrayData = Object.entries(dataDB.comments).map(([key, value])=> ({
-      //       idKey: key,
-      //       id: value.id,
-      //       name: value.name,
-      //       email: value.email,
-      //       msg: value.msg,
-      //       //imgAvatar: value.imgAvatar
-      //     }));
-      //     console.log('arrayData',arrayData);
-      //     setData(arrayData);
-      //   }
-    });
+  //   if (dataDB !== null) {
+  //     Object.values(dataDB.comments).map((comment) => {
+  //       setData((oldComment) => [...oldComment, comment]);
+  //     });
+  //   }
   }, []);
 
-  console.log("arrayData-data", data);
-
-  const AddComments = (e) => {
+  const AddComments = async (e) => {
     e.preventDefault();
-    const id = uid();
+    //const id = uid();
     const name = nameInputRef.current.value;
     const email = emailInputRef.current.value;
     const msg = textInputRef.current.value;
 
-    set(ref(db, `comments/${id}`), {
-      id,
-      name,
-      email,
-      msg,
-    });
+    //Realtime database add
+    // set(ref(db, `comments/${id}`), { id, name, email, msg, });
 
-    //const commentsRef = ref(db, "comments");
-
-    // const newCommentRef = push(commentsRef);
-    // set(newCommentRef, { id, name, email, msg });
+    try {
+      const docRef = await addDoc(collection(dbStore, "comments"), { name, email, msg, });
 
     nameInputRef.current.value = "";
     emailInputRef.current.value = "";
     textInputRef.current.value = "";
+    console.log('docRef', docRef.id)
+  } catch (error) {
+    console.error("Chyba pri pridávaní komentára:", error);
+  }
   };
 
-  const deleteComment = (comment) => {
-    remove(ref(db, `comments/${comment.id}`));
+  const deleteComment = async(id) => {
+    const docRef = doc(dbStore, "comments", id);
+    await deleteDoc(docRef);
+
+    //Realtime database delete
+    //remove(ref(db, `comments/${comment.id}`));
   };
 
-  ////Priamy fetch na url
-  //console.log("reformattedArray", reformattedArray); // [{ 1: 10 }, { 2: 20 }, { 3: 30 }]
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const result = await fetch(
-  //       "https://jsonplaceholder.typicode.com/comments"
-  //     ).then((response) => response.json());
-  //     setData(result);
-  //   };
-  //   fetchData();
-  // }, []);
-
-  // const fetchUrl =
-  //   "https://users-comments-1e926-default-rtdb.europe-west1.firebasedatabase.app/comments.json";
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const result = await fetch(fetchUrl).then((response) => response.json());
-  //     //console.log("result", result);
-  //     let dataArray = [];
-  //     for (var key in result) {
-  //       dataArray.push(result[key]);
-  //       //console.log("dataArray", dataArray);
-  //     }
-  //     setComments(dataArray);
-  //   };
-  //   fetchData();
-  // }, []);
+  
 
   return (
     <div className="container">
@@ -149,7 +120,7 @@ function Comments() {
             <p>{comment.msg}</p>
           </div>
           <div>
-            <button onClick={() => deleteComment(comment)}>Delete</button>
+            <button onClick={() => deleteComment(comment.id)}>Delete</button>
           </div>
         </div>
       ))}
@@ -161,52 +132,87 @@ export default Comments;
 
 //https://www.youtube.com/watch?v=azdwN_4IDKA&t=36s
 //https://www.youtube.com/watch?v=UYjntA5btvk&list=PLD8nQCAhR3tTXYOhau-RTfZQ4uGvco7XQ&index=16
+// https://www.youtube.com/watch?v=uVPtYLGPL80&list=PLqFvlDFoiZ-2SAX7YXCYtb28K4IooCIlS&index=4
 
-// import React, { useRef, useState } from 'react';
+/**/
+      //  onSnapshot(collection(dbStore, "comments"), (snapshot) => {
+      //   const comments = snapshot.docs.map((comment) => {
+      //     const data = comment.data();
+      //     const id = comment.id;
+      //     console.log('id', id);
+      //     return { ...data, id };
+      //   });
+      //   console.log('comments', comments);
+      //   if (comments !== null) {
+      //     setData(comments);
+      //   }
+      // }); 
 
-// function TodoList() {
-//   const [todos, setTodos] = useState([]);
-//   const todoInputRef = useRef();
-//   const priorityInputRef = useRef();
+//useEffect(() => {
+  // const commentsData = onSnapshot(collection(dbStore, "comments"), (snapshot) => {
+  //   const comments = [];
+  //   snapshot.forEach((doc) => {
+  //     comments.push(doc.data());
+  //   });
+  //   setData(comments)
+    
+  // });
+  // return () => {commentsData()};
 
-//   const addTodo = (event) => {
-//     event.preventDefault();
-//     const todo = todoInputRef.current.value;
-//     const priority = priorityInputRef.current.value;
-//     if (todo.trim() !== '') {
-//       const newTodo = {
-//         id: Date.now(),
-//         todo,
-//         priority
-//       };
-//       setTodos((prevTodos) => [...prevTodos, newTodo]);
-//       todoInputRef.current.value = '';
-//       priorityInputRef.current.value = '';
-//     }
-//   };
+  /*** Pre konfiguraciu Firebase - realtime database****/
+  // onValue(ref(db), (snapshot) => {
+  //   const dataDB = snapshot.val();
+  //   setData([]);
+  //   console.log("dataDB", dataDB);
 
-//   const deleteTodo = (id) => {
-//     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
-//   };
+  //   if (dataDB !== null) {
+  //     Object.values(dataDB.comments).map((comment) => {
+  //       setData((oldComment) => [...oldComment, comment]);
+  //     });
+  //   }
 
-//   return (
-//     <div>
-//       <h1>Todo List</h1>
-//       <form onSubmit={addTodo}>
-//         <input type="text" placeholder="Todo" ref={todoInputRef} />
-//         <input type="text" placeholder="Priority" ref={priorityInputRef} />
-//         <button type="submit">Add Todo</button>
-//       </form>
-//       <ul>
-//         {todos.map((todo) => (
-//           <li key={todo.id}>
-//             {todo.todo} - {todo.priority}
-//             <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
+    //   if(dataDB !== null) {
+    //     const arrayData = Object.entries(dataDB.comments).map(([key, value])=> ({
+    //       idKey: key,
+    //       id: value.id,
+    //       name: value.name,
+    //       email: value.email,
+    //       msg: value.msg,
+    //       //imgAvatar: value.imgAvatar
+    //     }));
+    //     console.log('arrayData',arrayData);
+    //     setData(arrayData);
+    //   }
+  //});
+  
+//}, []);
 
-// export default TodoList;
+
+////Priamy fetch na url
+  //console.log("reformattedArray", reformattedArray); // [{ 1: 10 }, { 2: 20 }, { 3: 30 }]
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const result = await fetch(
+  //       "https://jsonplaceholder.typicode.com/comments"
+  //     ).then((response) => response.json());
+  //     setData(result);
+  //   };
+  //   fetchData();
+  // }, []);
+
+  // const fetchUrl =
+  //   "https://users-comments-1e926-default-rtdb.europe-west1.firebasedatabase.app/comments.json";
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const result = await fetch(fetchUrl).then((response) => response.json());
+  //     //console.log("result", result);
+  //     let dataArray = [];
+  //     for (var key in result) {
+  //       dataArray.push(result[key]);
+  //       //console.log("dataArray", dataArray);
+  //     }
+  //     setComments(dataArray);
+  //   };
+  //   fetchData();
+  // }, []);
