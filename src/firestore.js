bd785e79a7c2bf9455b85b1d86b9f751058d009e
Petra poliakova <petra.poliakova@dxc.com>
2023-05-30 12:51:13 +0200

    added firestore

---
+++ src/firestore.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCqcrszk7pEWGrHWBMveYQVJxxYq8jsOlk",
+    authDomain: "users-comments-1e926.firebaseapp.com",
+    databaseURL: "https://users-comments-1e926-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "users-comments-1e926",
+    storageBucket: "users-comments-1e926.appspot.com",
+    messagingSenderId: "860609586306",
+    appId: "1:860609586306:web:368d65c7965b59e60c009b"
+  };
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+  
+const dbStore = getFirestore(app);
+
+export default dbStore;
+
+//export default getFirestore();
